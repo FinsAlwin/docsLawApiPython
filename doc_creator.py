b025b733711631adf0a2e0a23868995682c14844
FinsAlwin <alwinwk101@live.com>
2023-02-24 15:52:51 +0530

    eb version

--- doc_creator.py
+++ doc_creator.py
@@ -8,22 +8,22 @@ class DocCreator:
     def __init__(self, isUrgent, indexList, placeholderList, file_name):
 
         self.dest_doc = WordProcessor.getDocx('')
-        self.index = WordProcessor.getDocx('template/index.docx')
-        self.urgent = WordProcessor.getDocx('template/urgent.docx')
+        self.index = WordProcessor.getDocx('sampleTemplate/index.docx')
+        self.urgent = WordProcessor.getDocx('sampleTemplate/urgent.docx')
         self.notice_of_motion = WordProcessor.getDocx(
-            'template/notice_of_motion.docx')
+            'sampleTemplate/notice_of_motion.docx')
 
         self.memo_of_parties = WordProcessor.getDocx(
-            'template/memo_of_parties.docx')
+            'sampleTemplate/memo_of_parties.docx')
 
         self.pet_synopsis = WordProcessor.getDocx(
-            'template/pet_synopsis.docx')
+            'sampleTemplate/pet_synopsis.docx')
 
         self.pet_title = WordProcessor.getDocx(
-            'template/pet_title.docx')
+            'sampleTemplate/pet_title.docx')
 
         # self.vakalatnama = WordProcessor.getDocx(
-        #     'template/vakalatnama.docx')
+        #     'sampleTemplate/vakalatnama.docx')
 
         table_index = self.index.tables[1]
         for i, item in enumerate(indexList, start=1):
